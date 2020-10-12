769211f47594bf32a8f47a27dc1d57eb8f437f66
collingraves34 <collin.graves@stu.barren.kyschools.us>
2020-10-12 13:24:23 -0500

    JS Exercise

---
+++ jsBasicsExercise.js
@@ -0,0 +1,55 @@
+console.log("Favorite Number: " + 2);
+
+//Numbers
+25; //Whole Number
+2.5; // Float/Decimal
+-5; // Negative Number
+
+//Strings
+"This is the way!";
+'I have spoken!';
+"10"; 
+
+//Booleans
+true;
+false;
+
+//Null and undefinded
+null;
+undefined;
+
+//Operator symbols
+// Add +
+// Sub -
+// Multiply *
+// Divide /
+
+
+//Modulo
+console.log(3%7);
+console.log(7%3);
+
+//Mary Popins
+console.log('I’M MARY POPPINS, Y\'ALL!');
+
+//Spider Man
+console.log('Spider' + 'Man');
+
+//Name
+console.log("Collin".length + "Graves".length)
+
+//Index
+console.log("Avengers: Endgame"[16]);
+
+
+//BONUS
+console.log('\\');
+
+console.log(5, 4, 9);
+
+console.log("CAP\nVISION\nHAWKEYE\nNICK FURY\nGROOT\nDOCTOR STRANGE\nTHOR\nSTAR-LORD")
+
+var x = 10;
+var y = 5;
+var c = x + y;
+console.log(c);
